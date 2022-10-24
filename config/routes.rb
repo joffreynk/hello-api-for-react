05592f0020c93cf6fd4e54f87c881016e2d74972
joffreynk <joyjoffrey@gmail.com>
2022-10-24 23:19:44 +0200

    modify urli

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'greetings_api/index', to: "greetings_api#index"
+  get 'greetings_api', to: "greetings_api#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
